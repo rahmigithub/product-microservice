92facbd11507f539c5132f67d4b947d95d6352b0
rahmigithub <alkanrahmi93@gmail.com>
2024-07-27 04:47:00 +0300

    Deneme (Dikkate almayın)

--- src/main/java/org/alkan/stockmanager/productservice/configuration/SwaggerConfiguration.java
+++ src/main/java/org/alkan/stockmanager/productservice/configuration/SwaggerConfiguration.java
@@ -17,7 +17,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.alkan"))
+                .apis(RequestHandlerSelectors.basePackage("org.alkanmmm"))
                 .paths(PathSelectors.any())
                 .build();
     }
