ce30c1e0faee4a6106d54ebf9e9a30bd85d59872
rahmigithub <alkanrahmi93@gmail.com>
2024-07-27 04:48:22 +0300

    Revert "Deneme (Dikkate almayın)"
    This reverts commit 92facbd11507f539c5132f67d4b947d95d6352b0.

--- src/main/java/org/alkan/stockmanager/productservice/configuration/SwaggerConfiguration.java
+++ src/main/java/org/alkan/stockmanager/productservice/configuration/SwaggerConfiguration.java
@@ -17,7 +17,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.alkanmmm"))
+                .apis(RequestHandlerSelectors.basePackage("org.alkan"))
                 .paths(PathSelectors.any())
                 .build();
     }
